7b63c93e3423d70df47881bea8da73db0bb75c39
javacha <javacha@gmail.com>
2022-07-04 16:28:47 -0300

    inicio

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/javacha/go-mod1
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,14 @@
+package modExample1
+
+import (
+	"fmt"
+)
+
+func main() {
+	Saludar("gil")
+}
+
+func Saludar(nombre string) {
+
+	fmt.Println("hola ", nombre, ", como andas??")
+}
